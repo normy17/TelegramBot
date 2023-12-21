761207152f8646d41c5f39995d16f710a48a628f
normy17 <normy17@mail.ru>
2023-12-21 20:29:08 +0300

    Добавил кнопки для домашки

--- handlers/users/hello.py
+++ handlers/users/hello.py
@@ -1,10 +1,21 @@
+import datetime
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 import keyboards.default as kb
-from loader import dp
+from loader import dp, bot
 
 
 @dp.message_handler(text='Привет')
 async def bot_hello(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.username}', reply_markup=kb.keybords.hello_kb)
+    await message.answer(f'Привет, {message.from_user.username}', reply_markup=kb.keybords.inline_kb)
+
+
+@dp.callback_query_handler(lambda b: b.data == 'hello_btn')
+async def button_check(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    days = ['Понедельник', 'Вторник', 'Среда', 'Четверг']
+    today = datetime.datetime.now().weekday()
+
+    await bot.send_message(callback_query.from_user.id, f'{days[today]}')
--- keyboards/default/keybords.py
+++ keyboards/default/keybords.py
@@ -1,7 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
+couches = ['тренер 1', 'тренер 2']
+inline_kb = InlineKeyboardMarkup()
+for couch in couches:
+    button = InlineKeyboardButton(couch, callback_data='hello_btn')
+    inline_kb.add(button)
 
-button_hello = KeyboardButton('Привет')
-
-hello_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hello)
 
