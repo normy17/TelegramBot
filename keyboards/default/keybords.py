e9cb58b46934fc2b060c660bad6aaeac15e68227
normy17 <normy17@mail.ru>
2023-12-24 18:38:05 +0300

    Домашка, часть 2

--- keyboards/default/keybords.py
+++ keyboards/default/keybords.py
@@ -1,9 +1,14 @@
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
-couches = ['тренер 1', 'тренер 2']
+coaches = ['Иванов А.П.', 'Петров И.А', 'Соболев П.И']
 inline_kb = InlineKeyboardMarkup()
-for couch in couches:
-    button = InlineKeyboardButton(couch, callback_data='hello_btn')
+for coach in coaches:
+    button = InlineKeyboardButton(coach, callback_data=f'coach_{coach}')
     inline_kb.add(button)
 
 
+group_classes = ['Аэробика', 'Силовая тренировка', 'Кардио-тренировка', 'Функциональная тренировка', 'Зумба', 'Пилатес']
+inline_kb2 = InlineKeyboardMarkup()
+for group_class in group_classes:
+    button = InlineKeyboardButton(group_class, callback_data=f'class_{group_class}')
+    inline_kb2.add(button)
